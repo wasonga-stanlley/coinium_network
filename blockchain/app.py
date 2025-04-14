1f343e0ddd6e6c8232c59723bca34c2a4194f962
stanlley-locke <stanlleylocke@gmail.com>
2025-04-14 22:01:11 +0300

    Update app.py

--- blockchain/app.py
+++ blockchain/app.py
@@ -247,4 +247,4 @@ def route_show_blockchain():
     return jsonify(chain)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0' port='5000')
+    app.run(debug=True, host='0.0.0.0', port=5000)
